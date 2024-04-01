17d8b06a6562c4d648a6d761d91ede5c8155f180
nghiaho310pf <nghia.ho310pf@hcmut.edu.vn>
2024-04-01 14:14:54 +0700

    Basic SmartTextField to be CSS'ed later

---
+++ src/lib/ui/SmartTextField.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    /**
+     * @type {String}
+     */
+    export let title;
+    /**
+     * @type {String}
+     */
+    export let value;
+    /**
+     * @type {String | null}
+     */
+    export let error;
+
+    const dispatch = createEventDispatcher();
+</script>
+<div class="smart-text-field">
+    <input type="text" placeholder={title} bind:value={value}>
+
+    {#if error !== null}
+        {error}
+    {/if}
+</div>
