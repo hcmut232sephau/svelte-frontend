cea2a051dff064bff4c75a4a4cad665bda7dd23c
nghi3m <nghiem.nguyen@hcmut.edu.vn>
2024-04-25 14:34:37 +0700

    Add course entry to student's course browser

--- src/lib/dashboard/CourseBrowser.svelte
+++ src/lib/dashboard/CourseBrowser.svelte
@@ -43,7 +43,19 @@
         unsubscribeUserData();
         unsubscribeCourseData();
     });
+    let courseNames = ["Linear ALgebra", "Calculus", "Chemistry"]
+    let courseCodes = [1001, 1002, 1003]
+    let courseIndices = [0, 1, 2]
 </script>
 <div class="flex flex-col w-[50vw]">
+    {#each courseIndices as i}
+        <Card class="mx-auto mt-4 bg-neutral-800 border-none" size="md">
+            <div class="flex">
+                <span class="mx-3 my-auto">{courseCodes[i]}</span>
+                <span class="mx-2 my-auto text-white">{courseNames[i]}</span>
+                <Button class="w-48 ml-auto w-24">Join</Button>
+            </div>
+        </Card>
+    {/each}
 </div>
 
